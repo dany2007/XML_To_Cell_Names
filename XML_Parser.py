07f9e8d1d424da107d96676764d05725a30f87ac
Dan <dany200828@gmail.com>
2020-03-04 17:49:15 +0200

    *Check for path with no spaces and remove debug messages. *Fixed the empty line after the header in the .csv output

--- XML_Parser.py
+++ XML_Parser.py
@@ -4,7 +4,11 @@ import csv
 print("Drag an .XML file to extract cell names and ExternalId's")
 
 fileRaw = input()
-file = fileRaw[1:-1]
+if fileRaw[0] == '"':
+    file = fileRaw[1:-1]
+else:
+    file = fileRaw
+
 outputFile = file[:-4] + ".csv"
 tree = ET.parse(file)
 root = tree.getroot()
@@ -17,12 +21,10 @@ for cell in root.iter('RobcadStudy'):
         if content.tag == "name":
             cellDict[cell.attrib["ExternalId"]] = content.text
 
-# print(cellDict)
-
 csv_columns = ['ExternalId', 'Cell Name']
 csv_file = outputFile
 
-with open(csv_file, 'w') as csvfile:
+with open(csv_file, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
     writer.writeheader()
     for key in cellDict.keys():
